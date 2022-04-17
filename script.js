a60c136dfabaf04a399e3c26df20913c2100da07
Mateus Abelli <43862225+MateusAbelli@users.noreply.github.com>
2022-04-16 23:36:18 -0300

    Updated the script to include a delay

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const grid = document.querySelector(".grid");
 const cardTemplate = document.getElementById("card-template");
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 100; i++) {
   grid.append(cardTemplate.content.cloneNode(true));
 }
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((res) => res.json())
-  .then((posts) => {
-    grid.innerHTML = "";
-    posts.forEach((post) => {
-      const div = cardTemplate.content.cloneNode(true);
-      const img = div.querySelector("img");
-      img.addEventListener(
-        "load",
-        () => {
-          img.classList.remove("skeleton");
-        },
-        { once: true }
-      );
-      div.querySelector("[data-name]").textContent = post.title;
-      div.querySelector("[data-body]").textContent = post.body;
-      grid.append(div);
+setTimeout(() => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((posts) => {
+      grid.innerHTML = "";
+      posts.forEach((post) => {
+        const div = cardTemplate.content.cloneNode(true);
+        const img = div.querySelector("img");
+        img.src = "https://source.unsplash.com/100x100/?nature";
+        img.addEventListener(
+          "load",
+          () => {
+            img.classList.remove("skeleton");
+          },
+          { once: true }
+        );
+        div.querySelector("[data-title]").textContent = post.title;
+        div.querySelector("[data-body]").textContent = post.body;
+        grid.append(div);
+      });
     });
-  });
+}, 2500);
 
